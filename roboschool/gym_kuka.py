dc3b920ef604c67bee456ec08aeb6e5777001d16
Francesco Mannella <francesco.mannella@gmail.com>
2019-02-14 19:22:14 +0100

    modified angles and masses

--- roboschool/gym_kuka.py
+++ roboschool/gym_kuka.py
@@ -64,11 +64,13 @@ class PygletInteractiveWindow(pw.Window):
 def GraspRewardFunc(contact_dict, state):
     finger_reward = np.sum([ len([contact for contact in contacts 
         if not "table" in contact]) for part, contacts 
-        in contact_dict.items() if "finger" in part ])**2
+        in contact_dict.items() if "finger" in part ])
+
+    fingers_reward = len(np.unique(contact_dict.keys()))
 
     neg_reward = -np.sum([ len([contact for contact in contacts 
         if "table" in contact ]) for part, contacts 
-        in contact_dict.items() ])*0.1
+        in contact_dict.items() ])
     
     obj_pose = state[-3:]
     obj_reward = (
@@ -76,7 +78,7 @@ def GraspRewardFunc(contact_dict, state):
             -0.3 < obj_pose[1] < 0.3 and
                 0.8 < obj_pose[2] < 0.9)
 
-    return neg_reward + obj_reward + finger_reward + obj_reward*finger_reward**2
+    return neg_reward*0.5 + obj_reward**4 + finger_reward**6 + fingers_reward**6 + obj_reward*finger_reward**4
 
 
 
