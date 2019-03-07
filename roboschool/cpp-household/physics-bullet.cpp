240a5d98f5c55ca463aabffc4916be51f3ca9cc7
Francesco Mannella <francesco.mannella@gmail.com>
2019-03-07 14:47:32 +0100

    shared_memory

--- roboschool/cpp-household/physics-bullet.cpp
+++ roboschool/cpp-household/physics-bullet.cpp
@@ -2,6 +2,7 @@
 #include <QtWidgets/QApplication>
 #include <QtCore/QDir>
 #include <QtCore/QElapsedTimer>
+#include <fstream>
 
 namespace Household {
 
@@ -14,7 +15,18 @@ void World::bullet_init(float gravity, float timestep)
 	char* fake_argv[] = { 0 };
 	//client = b3CreateInProcessPhysicsServerAndConnectMainThread(0, fake_argv);
 	//client = b3CreateInProcessPhysicsServerAndConnect(0, fake_argv);
-	client = b3ConnectPhysicsDirect();
+    
+    // SHARED_MEMORY_KEY contains the memory key number to give to the server
+    // ${ROBOSCHOOL_PATH}/bullet3/build_cmake/examples/SharedMemory/App_PhysicsServer_SharedMemory --shared_memory_key=$(cat SHARED_MEMORY_KEY)
+    std::ifstream mkey("SHARED_MEMORY_KEY");
+    if(mkey.good()) {
+        int key;
+        mkey >> key;
+        client = b3ConnectSharedMemory(key);
+    }
+    else {
+        client = b3ConnectPhysicsDirect();
+    }
 	settings_gravity = gravity;
 	settings_timestep = timestep;
 	settings_apply();
